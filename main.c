8aacc0e64e9a5b6b7ba32dcc64ba2f550243061a
hugo-snunes <hugo.snunes@a.ucb.br>
2025-03-20 22:59:52 -0300

    add numericos

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    printf("GIT, GCC e VSCode instalados com sucesso!");
+    printf("");
     return 0;
 }
--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    int idade = 30;
+    int idade = 40;
     printf("A idade eh %d", idade); 
     return 0;
 }
