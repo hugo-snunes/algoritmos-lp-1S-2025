29e918f0ed945f903a92bb4ae5b8f3385001c065
hugo-snunes <hugo.snunes@a.ucb.br>
2025-03-29 16:18:59 -0300

    add atividade2

--- atividade2/trapezio.c
+++ atividade2/trapezio.c
@@ -10,14 +10,9 @@ float base_menor = 10;
 printf("\nDigite o valor do Altura:", altura);
 scanf("%f", &altura);
 
-printf("Digite o valor da Base maior:", base_maior);
-scanf("%f", &base_maior);
-
-printf("Digite o valor da Base menor:", base_menor);
-scanf("%f", &base_menor);
 
 printf("\nFormula: Area = (Base maior + Base menor) * Altura / 2\n");
-printf("\n A = (%.0f + %.0f) * %.0f / 2\n", base_maior, base_menor, altura);
+printf("\n A = (%.1f + %.1f) * %.1f / 2\n", base_maior, base_menor, altura);
 
 float area = (base_maior-base_menor)*altura/2;
 printf("\nO valor da area eh: %.1f", area);
