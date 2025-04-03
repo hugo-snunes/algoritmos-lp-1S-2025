c043e4ba70a7cd502aa5c0d28f6f3c7f44fdc9d2
hugo-snunes <hugo.snunes@a.ucb.br>
2025-04-03 11:54:18 -0300

    add atv1

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char* argv[]){
     float altura, peso;
 
     printf("CALCULADORA IMC\n");
     printf("\nDigite sua altura: ");
-    scanf("%f", &altura);
+     scanf("%f", &altura);
     printf("Digite seu peso: ");
      scanf("%f", &peso);
    
     printf("\nIMC = %.2f / (%.2f)^2\n", altura, peso);
 
-    float IMC = peso/(altura*altura);
-    printf("Seu IMC eh: %.2f", IMC);
+    float IMC = peso/ powf(altura, 2);
+    printf("Seu IMC eh: %.2f\n", IMC);
     
+if(IMC <= 18.5){
+    printf("Baixo peso\n");
+}
+
+if(IMC > 18.5 && IMC <= 24.9){
+    printf("Peso Normal\n");
+}
+
+if(IMC > 24.9 && IMC <= 29.9){
+    printf("Sobrepeso\n");
+}
+
+if(IMC > 29.9 && IMC <= 34.9){
+    printf("Obesidade I\n");
+}
+
+if(IMC > 34.9 && IMC <= 39.9){
+    printf("Obesidade II\n");
+}
+
+if(IMC > 39.9){
+    printf("Obesidade III\n");
+}
+
     return 0;
+
 }
