f67880f38179ccabdd49e522640646f2fe90a9aa
hugo-snunes <hugo.snunes@a.ucb.br>
2025-04-03 11:18:20 -0300

    add atv1

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    float altura = 1.67;
-    float peso = 58.72;
+    float altura, peso;
+
     printf("CALCULADORA IMC\n");
-    printf("Digite sua altura: ");
+    printf("\nDigite sua altura: ");
     scanf("%f", &altura);
     printf("Digite seu peso: ");
      scanf("%f", &peso);
-    printf("A altura eh %.2f\n", altura);
-    printf("O peso eh %.2f\n", peso); 
+   
+    printf("\nIMC = %.2f / (%.2f)^2\n", altura, peso);
 
     float IMC = peso/(altura*altura);
     printf("Seu IMC eh: %.2f", IMC);
