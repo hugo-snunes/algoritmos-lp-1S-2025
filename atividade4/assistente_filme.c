80ec67b774b3aa2a083e1cd277f2f256f6bd5f84
hugo-snunes <hugo.snunes@a.ucb.br>
2025-04-04 17:05:18 -0300

    add ass_v

--- atividade4/assistente_filme.c
+++ atividade4/assistente_filme.c
@@ -1,30 +1,66 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
+int tipo, genero, subgenero;
 
-char opcao_filme;
-char opcao_genero_leve =;
+printf("Ola! Sou sua assistente virtual para a escolha de filmes, como posso te ajudar?\n");
 
-printf("Olá! Sou uma assistente virtual para a escolha de filmes, como posso te ajudar?\n");
-
-printf("\nEscolha uma opcao: \n");
-printf("l - Filme Leve\n");
-printf("i - Filme Intenso\n");
-
-scanf("\n%c", &opcao_filme);
- 
-if(opcao_filme = l){
+printf("\nEscolha um tipo: \n");
+printf("1 - Filme Leve\n");
+printf("2 - Filme Intenso\n");
+   scanf("\n%d", &tipo);
+   
+if(tipo == 1){
 printf("Escolha o genero: \n");
-printf("c - Comedia\n");
-printf("a - Animacao\n");
-scanf("\n%c", opcao_genero_leve);
+printf("1 - Comedia\n");
+printf("2 - Animacao\n");
+   scanf("\n%d", &genero);
+   
+
+if(genero == 1){
+printf("Sugestao: As branquelas\n");
+}else if(genero == 2){
+printf("Sugestao: Toy Story\n");
+}else{
+printf("Opção Invalida, tente novamente.\n");
+}
+}
+if(tipo == 2){
+printf("Escolha o genero: \n");
+printf("1 - Terror\n");
+printf("2 - Acao\n");
+   scanf("%d", &genero);
+    
+if(genero == 1){
+printf("Escolha o subgenero: \n");
+printf("1 - Terror psicologico\n");
+printf("2 - Terror sobrenatural\n");
+   scanf("%d", &subgenero);
+   
+if(subgenero == 1){
+printf("Sugestao: Corra!\n");
+}else if(subgenero == 2){
+printf("Sugestao: Invocacao do Mal\n");
+}else{
+printf("Opção Invalida, tente novamente.\n");
+}
+}
+if(genero == 2){
+printf("Escolha o subgenero: \n");
+printf("1 - Super-herois\n");
+printf("2 - Aventura realista\n");
+   scanf("%d", &subgenero);
+   
+if(subgenero == 1){
+printf("Sugestao: Vingadores ultimato\n");    
+}else if(subgenero == 2){
+printf("Sugestao: Mad max: Estrada furia\n");
+}else{
+printf("Opção Invalida, tente novamente.\n");
 }
 
-
-
-
-
-
+}
+}
 
 return 0;
 
