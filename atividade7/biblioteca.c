39ea1c86c214d1ea42b0d0e7aeb8a707681d80db
hugo-snunes <hugo.snunes@a.ucb.br>
2025-05-30 17:35:57 -0300

    add maior/menor

--- atividade7/biblioteca.c
+++ atividade7/biblioteca.c
@@ -1,5 +1,3 @@
-//Retorna o maior entre 2 números inteiros
-
 int maior_2n(int a, int b){
 int maior;
     if(a > b){
@@ -22,7 +20,6 @@ int menor;
     return menor;
 }
 
-
 //Retorna o maior entre 3 números inteiros
 
 int maior_3n(int a, int b, int c){
@@ -33,23 +30,5 @@ int maior_3n(int a, int b, int c){
 
 int menor_3n(int a, int b, int c){
     return menor_2n(menor_2n(a, b), c);
-}
-
-//Retorna o maior entre N números inteiros
-
-int maior_n(int numeros[], int n){
-int maior = numeros[0];
-for(int i = 0; i < n; i++){
-    maior = maior_2n(maior, numeros[i]);
-}
-    return maior;
-}
-
-//Retorna se o número é primo ou não
-
-int primo_n(int a){
-float raiz_quadrada = sqrtf(a);
-int resto = raiz_quadrada % ;
-    if(int primo =! 0)
 }
 
---
+++ atividade7/biblioteca.h
@@ -0,0 +1,12 @@
+
+//Retorna o maior entre 2 números inteiros
+int maior_2n(int a, int b);
+
+//Retorna o menor entre 2 números inteiros
+int menor_2n(int a, int b);
+
+//Retorna o maior entre 3 números inteiros
+int maior_3n(int a, int b, int c);
+
+//Retorna o menor entre 3 números inteiros
+int menor_3n(int a, int b, int c);
